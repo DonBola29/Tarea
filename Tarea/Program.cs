f2a54a61dbd531953b71d95ce4197fdcd43aa1ea
BOLIVAR RICARDO RAMIREZ LAGOS <bolivar.ramirez@pucese.edu.ec>
2020-11-14 23:29:14 -0500

    Agregar archivos de proyecto.

---
+++ Tarea/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, impar = 0, par = 0;
+
+            for (i = 0; i < 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.Write("Los numeros pares son: {0}", i);
+                    impar++;
+
+                }
+            }
+        }
+    }
+}
